19689678d56b6d1fa405680ec94385c4625efb90
zuiurs <z11111001011@gmail.com>
2016-06-29 14:57:38 +0900

    finish making docker-image-clean.sh

--- util/docker-util/docker-image-clean.sh
+++ util/docker-util/docker-image-clean.sh
@@ -15,26 +15,31 @@ op_err_exit() {
 
 usage_exit() {
 	echo "Usage: ${CMDNAME} [-x] PREFIX" 1>&2
+	echo "Print command which deletes docker images you designate." 1>&2
+	echo "" 1>&2
+	echo "[Options]" 1>&2
+	echo "-x:	Execute delete command" 1>&2
 	exit 1
 }
 
-image_list() {
-	local images=`docker images | grep -e "^$1" | tr -s ' ' | cut -d' ' -f1,2 | tr ' ' ':'`
-	return "${images}"
+serialized_img_ls() {
+	local images=`docker images | grep -e "^$1" | tr -s ' ' | cut -d' ' -f1,2 | tr ' ' ':' | grep -v "REPOSITORY:TAG" | tr '\n' ' ' | perl -pe 's|^(.*)\s$|$1|'`
+	# TODO: The last perl one liner is really slow.
+	echo "${images}"
 }
 
-serializeid_img_ls() {
-	local images=`image_list $1 | tr '\n' ' '`
-	return "${images}"
+image_list() {
+	local images=`serialized_img_ls $1`
+	echo ${images} | tr ' ' '\n'
 }
 
 target_image_show() {
 	echo ------- Target Image -------
-	echo `image_list $1`
+	image_list $1
 	echo ----------------------------
 }
 
-while getopts hx OPT
+while getopts :hx OPT
 do
 	case ${OPT} in
 		h)	# HELP
@@ -52,13 +57,18 @@ shift $((OPTIND - 1))
 PREFIX=$1
 target_image_show ${PREFIX}
 
+SERIALIZED_IMG=`serialized_img_ls ${PREFIX}`
+
 if [ -z $FLAG_EX ]; then
-	echo docker rmi `serializeid_img_ls ${PREFIX}`
+	echo docker rmi ${SERIALIZED_IMG}
 else
-	echo -n Above images will delete, is it OK? [y/n]
-	read confirm
-	if [ confirm = "y" ]; then
-		echo docker rmi 'serializeid_img_ls ${PREFIX}`
+	echo -n "Above images will delete, is it OK? [y/n]: "
+	read confirm1
+	if [ ${confirm1} = "y" ]; then
+		echo -n "Are you sure? [y/n]: "
+		read confirm2
+		if [ ${confirm2} = "y" ]; then
+			echo docker rmi ${SERIALIZED_IMG}
+		fi
 	fi
 fi
-
